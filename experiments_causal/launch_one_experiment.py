2021be0266079397c3c84e3815194ac5ee70ae15
Vivian Nastl <vivian.nastl@tuebingen.mpg.de>
2024-05-07 16:35:11 +0200

    higher bid on cluster and slower launching

--- experiments_causal/launch_one_experiment.py
+++ experiments_causal/launch_one_experiment.py
@@ -268,4 +268,4 @@ if __name__ == "__main__":
                 launch_experiments_jobs(task=dic_args["task"], exp_obj=exp_obj)
                 success = True
             except:
-                sleep(600)
+                sleep(3600)
