2a7b3a0f5c1e7e23bc9f61f2add0105859c88416
Vivian Nastl <vivian.nastl@tuebingen.mpg.de>
2024-05-09 23:02:31 +0200

    fix error

--- experiments_causal/launch_experiments.py
+++ experiments_causal/launch_experiments.py
@@ -214,23 +214,23 @@ TASKS = [
 #     TASKS.append("sipp_random_test_" + f"{index}")
 
 
-for index in range(DIABETES_READMISSION_FEATURES_RANDOM_SUBSETS):
+for index in range(DIABETES_READMISSION_FEATURES_RANDOM_SUBSETS_NUMBER):
     TASKS.append("diabetes_readmission_random_test_" + f"{index}")
 
-for index in range(MEPS_FEATURES_RANDOM_SUBSETS):
+for index in range(MEPS_FEATURES_RANDOM_SUBSETS_NUMBER):
     TASKS.append("meps_random_test_" + f"{index}")
 
 
 for index in range(COLLEGE_SCORECARD_FEATURES_RANDOM_SUBSETS_NUMBER):
     TASKS.append("college_scorecard_random_test_" + f"{index}")
 
-for index in range(NHANES_LEAD_FEATURES_RANDOM_SUBSETS):
+for index in range(NHANES_LEAD_FEATURES_RANDOM_SUBSETS_NUMBER):
     TASKS.append("nhanes_lead_random_test_" + f"{index}")
 
-for index in range(PHYSIONET_FEATURES_RANDOM_SUBSETS):
+for index in range(PHYSIONET_FEATURES_RANDOM_SUBSETS_NUMBER):
     TASKS.append("physionet_random_test_" + f"{index}")
 
-for index in range(ASSISTMENTS_FEATURES_RANDOM_SUBSETS):
+for index in range(ASSISTMENTS_FEATURES_RANDOM_SUBSETS_NUMBER):
     TASKS.append("assistments_random_test_" + f"{index}")
 
 
